947767dc0e11140440c1e6d52c8ca96cbb30412c
svetlimladenov <svetlimladenov@gmail.com>
2022-11-19 17:10:13 +0200

    Add nvim configs

---
+++ .config/nvim/lua/user/colorscheme.lua
@@ -0,0 +1,12 @@
+-- vim.cmd "colorscheme darkplus"
+
+local colorscheme = "onedark"
+
+-- Protected call 
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme) 
+
+if not status_ok then
+    vim.notify("colorscheme " .. colorscheme .. " not found!")
+    return
+end
+
